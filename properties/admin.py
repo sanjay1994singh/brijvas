8628c220faf87a82c33999bc4df3f6179afe910e
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-04 19:30:44 +0530

    first

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Wishlist,
+    CompareProperty,
+    PropertyReview
+)
 
-# Register your models here.
+admin.site.register(Wishlist)
+admin.site.register(CompareProperty)
+admin.site.register(PropertyReview)
