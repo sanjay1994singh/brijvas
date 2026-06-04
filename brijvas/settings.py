d4a6d545f505c203ca1f861ab3a49befe7567ef0
sanjay1994singh <sanjaypython22@gmail.com>
2026-06-04 19:35:31 +0530

    first

--- brijvas/settings.py
+++ brijvas/settings.py
@@ -128,8 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
